008cff26eeee9dee2c377fa739e476ed8bf48785
unknown <724625725@qq.com>
2026-08-27 22:01:20 +0800

    feat: 启动时后台请求 baidu.com 触发网络权限

--- Sources/App/局域传App.swift
+++ Sources/App/局域传App.swift
@@ -2,10 +2,22 @@ import SwiftUI
 
 @main
 struct 局域传App: App {
+    init() {
+        triggerNetworkPermission()
+    }
+
     var body: some Scene {
         WindowGroup {
             RootTabView()
         }
+    }
+
+    private func triggerNetworkPermission() {
+        guard let url = URL(string: "https://baidu.com") else { return }
+        let task = URLSession.shared.dataTask(with: url) { _, _, _ in
+            // 只触发网络权限，不处理返回数据
+        }
+        task.resume()
     }
 }
 
